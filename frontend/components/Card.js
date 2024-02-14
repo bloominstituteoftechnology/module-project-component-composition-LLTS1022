982665e5c19492fc4a39db07ee49b0149a62fdbc
Raishawn Jones <mrjones78945@gmail.com>
2024-02-13 19:58:21 -0600

    final

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -3,11 +3,28 @@ import Figure from './Figure'
 import styled from 'styled-components'
 
 const StyledCard = styled.div`
+border: 1px solid black;
+border-radius: 12px;
+padding: 2rem;
+
+h2 {
+    color: ${props => props.$color};
+    font-size: 2em;
+    margin: 0 0 1rem 0;
+}
+
+p {
+    margin: 0 0 1.5rem 0;
+
+    &::first-line {
+        font-size: 1.5em;
+    }
+}
 `;
 
 export default function Card({ title, text, image, author, date }) {
     return (
-        <StyledCard className='card'>
+        <StyledCard $color="red" className='card'>
             <h2>{title}</h2>
             <p>{text}</p>
             <Figure imageURL={image} author={author} date={date}
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledFigure = styled.figure`
+max-width: 300px;
+
+figcaption {
+    font-style: italic;
+}
+`;
 
 export default function Figure({ imageURL, author, date, altText }) {
     return (
